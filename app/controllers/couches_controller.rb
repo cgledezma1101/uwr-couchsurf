7d902aca135285d0be79e17299b8c73a179f3cf5
Carlos Ledezma <cgledezma1101@gmail.com>
2016-07-30 12:55:57 +1000

    Initial commit of controllers to be used

---
+++ app/controllers/couches_controller.rb
@@ -0,0 +1,7 @@
+class CouchesController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'couches/create'
+
+  get 'couches/destroy'
+
+  get 'hosts/index'
+
+  get 'hosts/create'
+
+  get 'hosts/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :host, only: [:index, :create]
+  resources :host, only: [:index, :create, :destroy]
   resources :couch, only: [:create, :destroy]
 end
